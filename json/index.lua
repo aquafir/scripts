03ec412439eee32a79f923e3e6ceb310d3c34ce8
aquafirs@gmail.com <aquafirs@gmail.com>
2023-08-07 13:22:24 -0500

    Remove json dependency, built in

--- json/index.lua
+++ json/index.lua
@@ -1,6 +1,10 @@
 -----------------------LIBS-------------------------
 local fs = require("filesystem").GetScript()
-local json = require("json.lua")
+
+-- json.parse(jsonString) : Returns a table with the contents of the specified json string.
+-- json.serialize(table) : Returns a json string with the contents of the specified table.
+-- json.isNull(val) : Returns true if the value specified is a null read from a json
+-- json.null() : Returns a special value which is a representation of a null in a json
 
 --Create settings
 local settings = {}
@@ -9,7 +13,7 @@ settings['Name'] = "MyName"
 settings.Level = 123
 
 --Save settings
-local encoded = json.encode(settings)
+local encoded = json.serialize(settings)
 fs.WriteText("out.json", encoded)
 sleep(1000)
 
@@ -20,7 +24,7 @@ print(serialized)
 
 --Decode / print
 print('Decoding serialized settings...')
-local decoded = json.decode(serialized)
+local decoded = json.parse(serialized)
 
 for index, value in pairs(decoded) do
   print(index, value)  
